0199a373d900a60fb83bb053238e2cf03bf5d9e0
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-15 15:25:10 +0800

    画图

--- hello-py/rw_visual.py
+++ hello-py/rw_visual.py
@@ -1,13 +1,29 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-rw = RandomWalk()
-rw.fill_walk()
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
 
-plt.style.use('ggplot')
-fig, ax = plt.subplots()
+    point_numbers = list(range(rw.num_points))
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
+    ax.set_aspect('equal')
 
-plt.show()  
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+
+    plt.show()  
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+
+
