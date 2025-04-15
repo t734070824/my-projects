5d5c84dbb25b18e00bc7108736fc166c34e5d411
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-15 15:10:18 +0800

    feat: 随机

---
+++ hello-py/random_walk.py
@@ -0,0 +1,10 @@
+from random import choice
+
+class RandonWalk:
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        
