449d9839891eaedb8614cc6f99920424e335c469
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-15 11:04:13 +0800

    feat: 飞船左右移动

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,51 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
+                elif event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_q:
+                    sys.exit()
+                elif event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (135, 206, 235)
+
+        # Ship settings
+        # self.ship_speed_factor = 1.5
+
+        # # Bullet settings
+        # self.bullet_speed_factor = 3
+        # self.bullet_width = 3
+        # self.bullet_height = 15
+        # self.bullet_color = (60, 60, 60)
+        # self.bullets_allowed = 3
+
+        # # Alien settings
+        # self.alien_speed_factor = 1
+        # self.fleet_drop_speed = 10
+
+        # # Speed up the game
+        # self.speedup_scale = 1.1
+
+        # # Initialize dynamic settings
+        # self.initialize_dynamic_settings()
