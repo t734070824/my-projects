6f6b710530c0d3fa37c3fa8876d0628d13194dc7
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-16 17:06:21 +0800

    `

--- hello-py/python_repos.py
+++ hello-py/python_repos.py
@@ -3,7 +3,8 @@ import requests
 url = "https://api.github.com/search/repositories"
 url += "?q=language:python+sort:stars"
 
-r = requests.get(url)
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
 
 print(f"Status code: {r.status_code}")
 
