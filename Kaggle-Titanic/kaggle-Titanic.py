00a0c33eab403f97e741d04726aa6edbdefb4538
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-18 17:29:01 +0800

    1

--- Kaggle-Titanic/kaggle-Titanic.py
+++ Kaggle-Titanic/kaggle-Titanic.py
@@ -71,5 +71,55 @@ print(train_data[['Embarked', 'Survived']].groupby(['Embarked']).mean().sort_val
 #分割线
 print("=========================================")
 
-fig = px.scatter(train_data, x='Age', y='Fare', color='Survived', hover_data=['Sex', 'Pclass'], title='Age vs Fare')
-fig.show()
+# fig = px.scatter(train_data, x='Age', y='Fare', color='Survived', hover_data=['Sex', 'Pclass'], title='Age vs Fare')
+# fig.show()
+
+#提取乘客称谓到title中
+all_data['Title'] = all_data.Name.apply(lambda x: re.search(r'([A-Za-z]+)\.', x).group(1))
+
+print(all_data['Title'].value_counts())   
+
+
+
+# px.scatter(all_data, x="Title", y="Age", color="Sex",title="年龄、姓氏、性别分布图").show()
+
+#整合称谓信息
+all_data.loc[all_data.Title.isin(['Ms', 'Mlle']), 'Title'] = 'Miss'
+all_data.loc[all_data.Title.isin(['Mme', 'Mrs']), 'Title'] = 'Mrs'
+rare = ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona']
+all_data.loc[all_data.Title.isin(rare), 'Title'] = 'Rare'
+print(all_data['Title'].value_counts())   
+
+all_data.drop(['Name'], axis=1, inplace=True)
+
+#提取新特征
+# all_data['Family_Size'] = all_data['SibSp'] + all_data['Parch'] + 1
+# all_data['ticket_group_count']  = all_data.groupby('Ticket')['Ticket'].transform('count')
+# all_data['group_size'] = all_data[['Family_Size', 'ticket_group_count']].max(axis=1)
+# all_data['is_alone'] = all_data['group_size'].apply(lambda x: 1 if x == 1 else 0)
+
+all_data['family_size'] = all_data.SibSp + all_data.Parch + 1
+all_data['ticket_group_count'] = all_data.groupby('Ticket')['Ticket'].transform('count')
+all_data['group_size'] = all_data[['family_size', 'ticket_group_count']].max(axis = 1)
+all_data['is_alone'] = all_data.group_size.apply(lambda x: 1 if x == 1 else 0)
+
+
+#提取新特征，并填充缺失值
+all_data['fare_p'] = all_data['Fare'] / all_data.ticket_group_count
+all_data.loc[all_data[all_data.fare_p.isna()].index, 'fare_p'] = all_data['fare_p'].mean()
+
+
+#处理票价数据
+# px.scatter(all_data, x='Pclass', y='Fare', color='Pclass', title='票价，等级分布图').show()
+# fig = px.scatter(all_data, x="Pclass", y="Fare", color="Pclass",
+#                  title="票价，等级分布图")
+# fig.show()
+
+
+all_data.drop('Fare', axis=1, inplace=True)
+all_data.drop('Ticket', axis=1, inplace=True)
+
+#分割线
+print("=========================================")
+
+print(all_data[all_data.Embarked.isnull()])
