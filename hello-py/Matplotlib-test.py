aad4cee52614b882033d9241b44ef964f488c730
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-04-21 18:03:16 +0800

    feat: 神经网络

--- hello-py/Matplotlib-test.py
+++ hello-py/Matplotlib-test.py
@@ -153,3 +153,58 @@ def softmax(a):
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
+
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append(os.pardir)
+
+from mnist import load_mnist
+
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=False, flatten=True)
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)  
+
+img = x_train[0]
+label = t_train[0]
+
+# print(label)
+
+# print(img.shape)
+# img = img.reshape(28, 28)
+# print(img.shape)
+
+# img_show(img)
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=False, flatten=True)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
