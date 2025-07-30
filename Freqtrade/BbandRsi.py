b10ecef4fa91ac68e0b1c88bb793d2e33638fcb9
tang.qing/唐庆_沪_HQDV <qing.tang@51job.com>
2025-07-30 13:33:59 +0800

    策略

---
+++ Freqtrade/BbandRsi.py
@@ -0,0 +1,45 @@
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+class BbandRsi(IStrategy):
+
+    # config文件中如果有 "minimal_roi"则会重写下面的minimal_roi
+    # 1min 中后roi为正就继续持有，0min后roi大于-0.1%则继续持有。。。
+    # minimal_roi = {
+         "1": 0.00,
+         "0": -0.001
+    }
+    stoploss = -100
+    timeframe = '15m'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+
+        # Bollinger bands
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        dataframe['bb_lowerband'] = bollinger['lower']
+        dataframe['bb_middleband'] = bollinger['mid']
+        dataframe['bb_upperband'] = bollinger['upper']
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] < 30) &
+                    (dataframe['close'] < dataframe['bb_lowerband'])
+
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] > 70)
+
+            ),
+            'sell'] = 1
+        return dataframe
